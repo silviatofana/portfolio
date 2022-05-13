65ad1532e7cfb3b84db8da093ee839666b133900
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-05-13 11:10:11 +0300

    preserve-data-form

--- src/style.js
+++ src/style.js
@@ -215,3 +215,32 @@ form.addEventListener('submit', (event) => {
     displayMsg.style.visibility = 'hidden';
   }
 });
+
+let formData = {
+  full_name: '',
+  email: '',
+  message: '',
+};
+
+// retrieving the local storage
+if (localStorage.getItem('formData') !== null) {
+  const data = localStorage.getItem('formData');
+  formData = JSON.parse(data);
+}
+
+// saving data to local storage
+
+// getting form input fields and textarea
+const formElements = document.querySelectorAll('input, textarea');
+// looping through the elements
+formElements.forEach((element) => {
+  // displaying the formData values to input fields
+  element.value = formData[element.name];
+  // applying input event listener on elements
+  element.addEventListener('input', (e) => {
+    // setting the values of input fields to the respective keys in object
+    formData[e.target.name] = e.target.value;
+    // saving the data in local storage
+    localStorage.setItem('formData', JSON.stringify(formData));
+  });
+});
